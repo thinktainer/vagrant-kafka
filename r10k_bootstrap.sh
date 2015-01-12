149d005616f72d1c80b275632fd11ad3efa88c92
Martin Schinz <m.schinz@gmail.com>
2015-01-12 10:22:47 +0000

    remove r10k and add datacat module

--- r10k_bootstrap.sh
+++ r10k_bootstrap.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env bash
-if [[ ! `gem list r10k` ]];
-  then
-    echo "Installing r10k gem."
-    gem install --no-ri --no-rdoc r10k -v 1.4.1
-    echo "Done."
-  else
-    echo "r10k found."
-fi
-
 echo "Adding github to known hosts"
 KNOWN_HOSTS=/etc/ssh/ssh_known_hosts
 if [ ! -f $KNOWN_HOSTS ];
@@ -19,9 +10,4 @@ ssh-keygen -R github.com -f $KNOWN_HOSTS
 ssh-keyscan github.com > $KNOWN_HOSTS
 echo "Done."
 
-echo "Installing modules from Puppetfile."
-PUPPETFILE=/puppet/Puppetfile \
-PUPPETFILE_DIR=/puppet/modules \
-r10k puppetfile install
-echo "Done."
 
